2e150be053991adf5f598b3d7d5d7e9ea95adcd1
Ricaardooo <guillenricardo.1704@gmail.com>
2025-03-03 12:34:11 -0600

    commitPrueba

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo-cambio1-prueba-commit',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('You  have pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -120,3 +120,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+//cambio prueba
